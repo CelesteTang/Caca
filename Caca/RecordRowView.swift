7ab19cbf04cdea3cd86994e33739e7993c795434
CelesteTang <optimism2025@gmail.com>
2017-03-20 16:41:56 +0800

    Add basic files

---
+++ Caca/RecordRowView.swift
@@ -0,0 +1,9 @@
+//
+//  RecordRowView.swift
+//  Caca
+//
+//  Created by 湯芯瑜 on 2017/3/20.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import Foundation
---
+++ Caca/SettingRowView.swift
@@ -0,0 +1,9 @@
+//
+//  SettingRowView.swift
+//  Caca
+//
+//  Created by 湯芯瑜 on 2017/3/20.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import Foundation
