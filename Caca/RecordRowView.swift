289c4bbbd0d1c510457968f2240a116db1173496
CelesteTang <optimism2025@gmail.com>
2017-03-21 20:37:55 +0800

    Set up RecordRowView

--- Caca/RecordRowView.swift
+++ Caca/RecordRowView.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class RecordRowView: UIView {
+    
+    @IBOutlet weak var cacaPhotoImageView: UIImageView!
+
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var passOrFailLabel: UILabel!
+    
+}
