d622e80a5a421c07bd6f2b25ed9ba3ebb1dc752a
CelesteTang <optimism2025@gmail.com>
2017-03-28 15:46:38 +0800

    Add a calendar

---
+++ Caca/CalendarCellView.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCellView.swift
+//  Caca
+//
+//  Created by 湯芯瑜 on 2017/3/28.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCellView: JTAppleDayCellView {
+
+    @IBOutlet weak var dayLabel: UILabel!
+
+    @IBOutlet weak var cacaLabel: UIImageView!
+}
--- Caca/CalendarViewController.swift
+++ Caca/CalendarViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import JTAppleCalendar
 
 class CalendarViewController: UIViewController {
+
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "Calendar"
         view.backgroundColor = Palette.backgoundColor
+
+        calendarView.dataSource = self
+        calendarView.delegate = self
+        calendarView.registerCellViewXib(file: "CalendarCellView") // Registering your cell is manditory
+
+        calendarView.cellInset = CGPoint(x: 0, y: 0)
+
     }
+
+}
+
+extension CalendarViewController: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+
+        let startDate = formatter.date(from: "2016 02 01")! // You can use date generated from a formatter
+        let endDate = Date()                                // You can also use dates created from this function
+        let parameters = ConfigurationParameters(startDate: startDate,
+                                                 endDate: endDate,
+                                                 numberOfRows: 6, // Only 1, 2, 3, & 6 are allowed
+                                                 calendar: Calendar.current,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfGrid,
+                                                 firstDayOfWeek: .sunday)
+
+        return parameters
+    }
+
+    func calendar(_ calendar: JTAppleCalendarView, willDisplayCell cell: JTAppleDayCellView, date: Date, cellState: CellState) {
+
+        guard let myCustomCell = cell as? CalendarCellView else { return }
+
+        // Setup Cell text
+        myCustomCell.dayLabel.text = cellState.text
+
+        // Setup text color
+        if cellState.dateBelongsTo == .thisMonth {
+            myCustomCell.dayLabel.textColor = UIColor.black
+        } else {
+            myCustomCell.dayLabel.textColor = UIColor.gray
+        }
+    }
+}
+
+extension CalendarViewController {
+
+    // swiftlint:disable force_cast
+    class func create() -> CalendarViewController {
+
+        return UIStoryboard(name: "Calendar", bundle: nil).instantiateViewController(withIdentifier: "CalendarViewController") as! CalendarViewController
+
+    }
+    // swiftlint:enable force_cast
 
 }
