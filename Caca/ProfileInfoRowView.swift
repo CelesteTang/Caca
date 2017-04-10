916bc4fbd0d5388f8477ac1d78bec2e17105204e
CelesteTang <optimism2025@gmail.com>
2017-04-10 19:59:15 +0800

    Add ProfileInfoTableViewCellr

---
+++ Caca/ProfileInfoRowView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileInfoRowView.swift
+//  Caca
+//
+//  Created by 湯芯瑜 on 2017/4/10.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProfileInfoRowView: UIView {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+}
+
+extension ProfileInfoRowView {
+    
+    // swiftlint:disable force_cast
+    class func create() -> ProfileInfoRowView {
+        
+        return UINib(nibName: "ProfileInfoRowView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! ProfileInfoRowView
+    }
+}
+// swiftlint:enable force_cast
