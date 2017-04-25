7a23b7ce80a4c39b33b33ce3f0128f586ce428bc
CelesteTang <optimism2025@gmail.com>
2017-04-25 10:13:50 +0800

    Improve opening

--- Caca/OpeningViewController.swift
+++ Caca/OpeningViewController.swift
@@ -37,8 +37,11 @@ class OpeningViewController: UIViewController {
     @IBAction func goBack(_ sender: UIButton) {
 
         if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
-            appDelegate.window?.rootViewController = UIStoryboard(name: "Landing", bundle: nil).instantiateViewController(withIdentifier: "StartViewController") as? StartViewController
+            
+            let startViewController = UIStoryboard(name: "Landing", bundle: nil).instantiateViewController(withIdentifier: "StartViewController") as? StartViewController
 
+            appDelegate.window?.rootViewController = startViewController
+            
             isFromStart = false
             isFromSignUp = false
 
@@ -55,7 +58,9 @@ class OpeningViewController: UIViewController {
 
             UserDefaults.standard.set(true, forKey: "IsviewedWalkThrough")
 
-            appDelegate.window?.rootViewController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController
+            let tabBarController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController
+            
+            appDelegate.window?.rootViewController = tabBarController
 
         }
 
@@ -66,6 +71,7 @@ class OpeningViewController: UIViewController {
         switch index {
 
         case 0...1:
+            
             guard let pageViewController = parent as? OpeningPageViewController else { return }
             pageViewController.forward(index: index)
 
@@ -81,31 +87,41 @@ class OpeningViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUp()
+        
+    }
+    
+    // MARK: Set Up
+    
+    private func setUp() {
+    
         self.view.backgroundColor = Palette.lightblue2
-        self.openingLabel.textColor = Palette.darkblue
+        
         self.forwardButton.tintColor = Palette.darkblue
-
+        
+        self.openingLabel.textColor = Palette.darkblue
         self.openingLabel.text = heading
-        self.openingImage.image = UIImage(named: imageFile)
         self.openingLabel.font = UIFont(name: "Futura-Bold", size: 30)
-
+        
+        self.openingImage.image = UIImage(named: imageFile)
+        
         self.goBackButton.setTitle("", for: .normal)
         let buttonimage = #imageLiteral(resourceName: "goBack").withRenderingMode(.alwaysTemplate)
         self.goBackButton.setImage(buttonimage, for: .normal)
         self.goBackButton.tintColor = Palette.darkblue
-
+        
         self.startButton.setTitle("Start", for: .normal)
         self.startButton.tintColor = Palette.cream
         self.startButton.backgroundColor = Palette.darkblue2
         self.startButton.layer.cornerRadius = self.startButton.frame.height / 2
         self.startButton.titleLabel?.font = UIFont(name: "Futura-Bold", size: 20)
-
+        
         self.pageControl.currentPage = index
-
+        
         switch index {
-
+            
         case 0:
-
+            
             self.forwardButton.setTitle("Next", for: .normal)
             self.forwardButton.titleLabel?.font = UIFont(name: "Futura-Bold", size: 20)
             self.boy.isHidden = false
@@ -115,17 +131,17 @@ class OpeningViewController: UIViewController {
             self.startButton.isHidden = true
             self.startButton.isEnabled = false
             if isFromStart == true {
-
+                
                 self.goBackButton.isHidden = false
-
+                
             } else if isFromSignUp == true {
-
+                
                 self.goBackButton.isHidden = true
-
+                
             }
-
+            
         case 1:
-
+            
             self.forwardButton.setTitle("Next", for: .normal)
             self.forwardButton.titleLabel?.font = UIFont(name: "Futura-Bold", size: 20)
             self.goBackButton.isHidden = true
@@ -135,9 +151,9 @@ class OpeningViewController: UIViewController {
             self.cacaLogo.isHidden = true
             self.startButton.isHidden = true
             self.startButton.isEnabled = false
-
+            
         case 2:
-
+            
             self.forwardButton.setTitle("Done", for: .normal)
             self.forwardButton.titleLabel?.font = UIFont(name: "Futura-Bold", size: 20)
             self.goBackButton.isHidden = true
@@ -149,11 +165,9 @@ class OpeningViewController: UIViewController {
             self.startButton.isEnabled = true
             self.forwardButton.isHidden = true
             self.forwardButton.isEnabled = false
-
+            
         default: break
-
+            
         }
-
     }
-
 }
