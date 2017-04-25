96e196c6218cf6015a43cf36befa8676565e7add
CelesteTang <optimism2025@gmail.com>
2017-04-25 10:47:31 +0800

    Improve fillin

--- Caca/FillinTableViewController.swift
+++ Caca/FillinTableViewController.swift
@@ -191,10 +191,9 @@ class FillinTableViewController: UITableViewController {
                 self.amountSlider.setThumbImage(newImage, for: .normal)
 
             default: break
+                
             }
-
         }
-
     }
 
     func resizeImage(image: UIImage, targetRatio: CGFloat) -> UIImage {
@@ -482,7 +481,9 @@ class FillinTableViewController: UITableViewController {
 
         if isFromCaca == true {
 
-            if let appDelegate = UIApplication.shared.delegate as? AppDelegate, let tabBarController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController {
+            if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+                
+                let tabBarController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController
 
                 appDelegate.window?.rootViewController = tabBarController
 
@@ -515,6 +516,7 @@ class FillinTableViewController: UITableViewController {
         let cancelAction = UIAlertAction(title: "Cancel",
                                          style: .cancel,
                                          handler: nil)
+        
         alertController.addAction(cancelAction)
 
         let photoAction = UIAlertAction(title: "Choose from library", style: .default) { _ in
@@ -527,6 +529,7 @@ class FillinTableViewController: UITableViewController {
             self.present(picker, animated: true, completion: nil)
 
         }
+        
         alertController.addAction(photoAction)
 
         let cameraAction = UIAlertAction(title: "Take photo", style: .default) { _ in
@@ -542,6 +545,7 @@ class FillinTableViewController: UITableViewController {
             self.present(picker, animated: true, completion: nil)
 
         }
+        
         alertController.addAction(cameraAction)
 
         self.present(alertController, animated: true, completion: nil)
@@ -560,10 +564,7 @@ class FillinTableViewController: UITableViewController {
             let timeCell = tableView.visibleCells[Component.time.rawValue] as? InfoTableViewCell,
             let shapeCell = tableView.visibleCells[Component.shape.rawValue] as? InfoTableViewCell,
             let colorCell = tableView.visibleCells[Component.color.rawValue] as? InfoTableViewCell,
-            let amountCell = tableView.visibleCells[Component.amount.rawValue] as? InfoTableViewCell else {
-
-                return
-        }
+            let amountCell = tableView.visibleCells[Component.amount.rawValue] as? InfoTableViewCell else { return }
 
         if dateCell.rowView.infoTextField.text == "" {
 
@@ -762,9 +763,11 @@ class FillinTableViewController: UITableViewController {
 
     func switchToRecord() {
 
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate, let tabBarController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
 
-            tabBarController.selectedIndex = TabBarItemType.record.rawValue
+            let tabBarController = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarController") as? TabBarController
+            
+            tabBarController?.selectedIndex = TabBarItemType.record.rawValue
 
             appDelegate.window?.rootViewController = tabBarController
 
@@ -782,11 +785,7 @@ class FillinTableViewController: UITableViewController {
             let amountCell = tableView.visibleCells[Component.amount.rawValue] as? InfoTableViewCell,
             let otherCell = tableView.visibleCells[Component.other.rawValue] as? InfoTableViewCell,
             let finishCell = tableView.visibleCells[Component.finish.rawValue] as? FinishTableViewCell
-            else {
-
-                return
-
-        }
+            else { return }
 
         finishCell.rowView.finishButton.isEnabled = false
 
@@ -807,6 +806,8 @@ class FillinTableViewController: UITableViewController {
         let photoID = UUID().uuidString
         let overallAdvice = getAdvice()
 
+        // MARK : Create caca with photo
+        
         if photoCell.rowView.cacaPhotoImageView.image != #imageLiteral(resourceName: "caca-big") {
 
             CacaProvider.shared.saveCacaPhoto(of: photoCell.rowView.cacaPhotoImageView.image!, with: photoID, completion: { (cacaPhotoUrl, error) in
@@ -841,6 +842,8 @@ class FillinTableViewController: UITableViewController {
 
         } else {
 
+            // MARK : Create caca without photo
+            
             let value = ["host": hostUID,
                          "cacaID": cacaID,
                          "photo": "",
@@ -897,6 +900,8 @@ class FillinTableViewController: UITableViewController {
         let photoID = recievedCacaFromRecordDetail[0].photoID
         let overallAdvice = getAdvice()
 
+        // MARK : Edit caca with new photo (had old photo)
+        
         if photoCell.rowView.cacaPhotoImageView.image != #imageLiteral(resourceName: "caca-big") && recievedCacaFromRecordDetail[0].photoID != "" {
 
             CacaProvider.shared.editCacaPhoto(of: photoCell.rowView.cacaPhotoImageView.image!, with: photoID, completion: { (cacaPhotoUrl, storageError, deleteError) in
@@ -938,6 +943,8 @@ class FillinTableViewController: UITableViewController {
 
         } else if photoCell.rowView.cacaPhotoImageView.image != #imageLiteral(resourceName: "caca-big") && recievedCacaFromRecordDetail[0].photoID == "" {
 
+            // MARK : Edit caca with new photo (no old photo)
+            
             CacaProvider.shared.saveCacaPhoto(of: photoCell.rowView.cacaPhotoImageView.image!, with: photoID, completion: { (cacaPhotoUrl, error) in
 
                 if error != nil {
@@ -970,6 +977,8 @@ class FillinTableViewController: UITableViewController {
 
         } else if photoCell.rowView.cacaPhotoImageView.image == #imageLiteral(resourceName: "caca-big") {
 
+            // MARK : Edit caca without new photo (no old photo)
+            
             let value = ["host": hostUID,
                          "cacaID": cacaID,
                          "photo": "",
@@ -996,11 +1005,7 @@ extension FillinTableViewController: UIImagePickerControllerDelegate, UINavigati
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
 
-        guard let cell = tableView.visibleCells.first as? PhotoTableViewCell else {
-
-            return
-
-        }
+        guard let cell = tableView.visibleCells.first as? PhotoTableViewCell else { return }
 
         if let editedCacaImage = info[UIImagePickerControllerEditedImage] as? UIImage {
 
@@ -1048,8 +1053,7 @@ extension FillinTableViewController: UIPickerViewDataSource, UIPickerViewDelegat
 
         switch pickerView {
 
-        case timePicker:
-            return 3
+        case timePicker: return 3
 
         default: return 1
 
@@ -1064,27 +1068,22 @@ extension FillinTableViewController: UIPickerViewDataSource, UIPickerViewDelegat
         case timePicker:
 
             switch component {
-            case 0:
-                return hour.count
-            case 1:
-                return min.count
-            case 2:
-                return sec.count
-            default:
-                return 0
+                
+            case 0: return hour.count
+                
+            case 1: return min.count
+                
+            case 2: return sec.count
+                
+            default: return 0
+                
             }
 
-        case shapePicker:
+        case shapePicker: return shapes.count
 
-            return shapes.count
+        case colorPicker: return colors.count
 
-        case colorPicker:
-
-            return colors.count
-
-        default:
-
-            return 0
+        default: return 0
 
         }
 
@@ -1101,13 +1100,15 @@ extension FillinTableViewController: UIPickerViewDataSource, UIPickerViewDelegat
             case timePicker:
 
                 switch component {
-                case 0:
-                    finalHour = String(format: "%02i", hour[row])
-                case 1:
-                    finalMin = String(format: "%02i", min[row])
-                case 2:
-                    finalSec = String(format: "%02i", sec[row])
+                    
+                case 0: finalHour = String(format: "%02i", hour[row])
+                    
+                case 1: finalMin = String(format: "%02i", min[row])
+                    
+                case 2: finalSec = String(format: "%02i", sec[row])
+                    
                 default: break
+                    
                 }
 
             timeCell.rowView.infoTextField.text = "\(finalHour):\(finalMin):\(finalSec)"
@@ -1136,13 +1137,18 @@ extension FillinTableViewController: UIPickerViewDataSource, UIPickerViewDelegat
             let pickerLabel = UILabel()
 
             switch component {
+                
             case 0:
                 pickerLabel.text = String(format: "%02i", hour[row])
+                
             case 1:
                 pickerLabel.text = String(format: "%02i", min[row])
+                
             case 2:
                 pickerLabel.text = String(format: "%02i", sec[row])
+                
             default: break
+                
             }
 
             pickerLabel.font = UIFont(name: "Futura-Bold", size: 20)
@@ -1184,17 +1190,23 @@ extension FillinTableViewController: UIPickerViewDataSource, UIPickerViewDelegat
 extension String {
 
     func index(from: Int) -> Index {
+        
         return self.index(startIndex, offsetBy: from)
+        
     }
 
     func substring(from: Int) -> String {
+        
         let fromIndex = index(from: from)
         return substring(from: fromIndex)
+        
     }
 
     func substring(to: Int) -> String {
+        
         let toIndex = index(from: to)
         return substring(to: toIndex)
+        
     }
 
 }
