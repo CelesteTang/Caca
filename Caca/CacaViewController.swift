0f12fbd344939791f5b61a67c812ff3052922dc6
CelesteTang <optimism2025@gmail.com>
2017-05-03 20:22:13 +0800

    Fix bugs

--- Caca/CacaViewController.swift
+++ Caca/CacaViewController.swift
@@ -49,8 +49,20 @@ class CacaViewController: UIViewController {
         detectFrequency()
 
         prepareNotification()
+        
+        let button = UIButton(type: .roundedRect)
+        button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
+        button.setTitle("Crash", for: [])
+        button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
+        view.addSubview(button)
+
     }
 
+    @IBAction func crashButtonTapped(_ sender: AnyObject) {
+        Crashlytics.sharedInstance().crash()
+    }
+
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
