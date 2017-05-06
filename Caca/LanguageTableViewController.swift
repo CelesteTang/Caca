9faed83f8b58791773bf228a22c9340342fbdc5f
CelesteTang <optimism2025@gmail.com>
2017-05-06 20:47:11 +0800

    Fix isPass bug

--- Caca/LanguageTableViewController.swift
+++ Caca/LanguageTableViewController.swift
@@ -118,43 +118,10 @@ class LanguageTableViewController: UITableViewController {
         case .English: UserDefaults.standard.set(["en"], forKey: "AppleLanguages")
         case .繁體中文: UserDefaults.standard.set(["zh-Hant"], forKey: "AppleLanguages")
         }
-
-        if let appDelegate = UIApplication.shared.delegate as? AppDelegate, let tabBarController = UIStoryboard(name: Constants.Storyboard.tabBar, bundle: nil).instantiateViewController(withIdentifier: Constants.Identifier.tabBar) as? TabBarController {
-
-            appDelegate.window?.rootViewController = tabBarController
-            appDelegate.window?.makeKeyAndVisible()
-
-        }
+    }
+    
+    func showAlert() {
+    
+        let alertController = UIAlertController(title: "Note", message: "Please restart", preferredStyle: .actionSheet)
     }
 }
-
-//class L012Localizer: NSObject {
-//    
-//    class func DoTheSwizzling() {
-//        // 1
-//        MethodSwizzleGivenClassName(cls: Bundle.self, originalSelector: #selector(Bundle.localizedString(forKey:value:table:)), overrideSelector: Selector(("specialLocalizedStringForKey:value:table:")))
-//    }
-//}
-//extension Bundle {
-//    func specialLocalizedStringForKey(key: String, value: String?, table tableName: String?) -> String {
-//        /*2*/let currentLanguage = L102Language.currentAppleLanguage()
-//        var bundle = Bundle()
-//        /*3*/if let _path = Bundle.main.path(forResource: currentLanguage, ofType: "lproj") {
-//            bundle = Bundle(path: _path)!
-//        } else {
-//            let _path = Bundle.main.path(forResource: "Base", ofType: "lproj")!
-//            bundle = Bundle(path: _path)!
-//        }
-//        /*4*/return (bundle.specialLocalizedStringForKey(key: key, value: value, table: tableName))
-//    }
-//}
-///// Exchange the implementation of two methods for the same Class
-//func MethodSwizzleGivenClassName(cls: AnyClass, originalSelector: Selector, overrideSelector: Selector) {
-//    let origMethod: Method = class_getInstanceMethod(cls, originalSelector)
-//    let overrideMethod: Method = class_getInstanceMethod(cls, overrideSelector)
-//    if (class_addMethod(cls, originalSelector, method_getImplementation(overrideMethod), method_getTypeEncoding(overrideMethod))) {
-//        class_replaceMethod(cls, overrideSelector, method_getImplementation(origMethod), method_getTypeEncoding(origMethod))
-//    } else {
-//        method_exchangeImplementations(origMethod, overrideMethod)
-//    }
-//}
